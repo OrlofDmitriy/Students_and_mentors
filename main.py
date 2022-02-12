a7b81282c3ba47e5fe141729c0efccb03512c04d
Дмитрий Орлов <orlof@mail.ru>
2022-02-13 01:19:48 +0500

    Task.3.1.Changing the __str__ method

--- main.py
+++ main.py
@@ -11,13 +11,31 @@ class Student:
         self.finished_courses.append(course_name)
 
     def rate_lect(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress \
+                and course in lecturer.courses_attached:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def _average_rating(self):
+        gr = []
+        count = 0
+        for values in self.grades.values():
+            for value in values:
+                gr.append(value)
+                count += 1
+        res = round(sum(gr) / count, 1)
+        return res
+
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\n' \
+            f'Средняя оценка за домашние задания: {self._average_rating()}\n' \
+            f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n' \
+            f'Завершенные курсы: {", ".join(self.finished_courses)}'
+        return res
 
 
 class Mentor:
@@ -32,6 +50,20 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
+    def _average_rating(self):
+        gr = []
+        count = 0
+        for values in self.grades.values():
+            for value in values:
+                gr.append(value)
+                count += 1
+        res = round(sum(gr) / count, 1)
+        return res
+
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self._average_rating()}'
+        return res
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -45,3 +77,7 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}'
+        return res
