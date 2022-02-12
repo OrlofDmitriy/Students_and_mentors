fc099ce4ff789c51c384d17a65a8b50349b193ea
Дмитрий Орлов <orlof@mail.ru>
2022-02-12 20:31:26 +0500

    Task.1. Added Lecturer Reviewer classes

--- main.py
+++ main.py
@@ -27,14 +27,11 @@ class Mentor:
             return 'Ошибка'
 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-
-print(best_student.grades)
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
